d2de8cbdf14f3c1aee7ce471ba874f0068dc5062
Ismayil Khayredinov <ismayil.khayredinov@gmail.com>
2016-02-15 17:34:04 +0100

    fix(sidebar): featured module now respects subtype configuration

--- views/default/groups/sidebar/featured.php
+++ views/default/groups/sidebar/featured.php
@@ -7,10 +7,13 @@
 
 $identifier = elgg_extract('identifier', $vars, 'groups');
 
+$subtypes = is_callable('group_subtypes_get_subtypes') ? group_subtypes_get_subtypes($identifier) : ELGG_ENTITIES_ANY_VALUE;
+
 $featured_groups = elgg_get_entities_from_metadata(array(
 	'metadata_name' => 'featured_group',
 	'metadata_value' => 'yes',
 	'type' => 'group',
+	'subtypes' => $subtypes,
 ));
 
 if ($featured_groups) {
